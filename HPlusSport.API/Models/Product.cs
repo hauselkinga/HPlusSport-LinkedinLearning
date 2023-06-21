04e97263aba9bfa8ec9e1f6b6de3470a1e4ea55b
Hausel Kinga <hausel.kinga@gmail.com>
2023-06-21 19:44:26 +0200

    add inmemory database with seed

--- HPlusSport.API/Models/Product.cs
+++ HPlusSport.API/Models/Product.cs
@@ -9,7 +9,7 @@ namespace HPlusSport.API.Models
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public bool IsAvaiable { get; set; }
+        public bool IsAvailable { get; set; }
 
         public int CategoryId { get; set; }
         [JsonIgnore]
