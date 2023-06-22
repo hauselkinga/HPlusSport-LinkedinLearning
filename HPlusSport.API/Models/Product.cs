dfe839edb0edb107115f9505bcf808a410046b10
Hausel Kinga <hausel.kinga@gmail.com>
2023-06-22 10:23:55 +0200

    define required fields for Product

--- HPlusSport.API/Models/Product.cs
+++ HPlusSport.API/Models/Product.cs
@@ -6,9 +6,13 @@ namespace HPlusSport.API.Models
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Sku { get; set; } = string.Empty;
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string Description { get; set; } = string.Empty;
+        [Required]
         public decimal Price { get; set; }
         public bool IsAvailable { get; set; }
 
