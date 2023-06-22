86767d4ee4240a0d25927e6c3b9b54916cc6a0dd
Hausel Kinga <hausel.kinga@gmail.com>
2023-06-22 10:34:58 +0200

    add PutProduct method with error handling

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -41,11 +41,6 @@ namespace HPlusSport.API.Controllers
         [HttpPost]
         public async Task<ActionResult> PostProduct(Product product)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest();
-            }
-
             _context.Products.Add(product);
             await _context.SaveChangesAsync();
 
@@ -53,6 +48,35 @@ namespace HPlusSport.API.Controllers
                 "GetProduct",
                 new { id = product.Id },
                 product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(p => p.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
     }
 }
