6ac308f15644dbd6f6dfc99247282b23d8046831
Hausel Kinga <hausel.kinga@gmail.com>
2023-06-26 09:29:23 +0200

    add pagination to products

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -19,10 +19,15 @@ namespace HPlusSport.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult> GetAllProducts()
+        public async Task<ActionResult> GetAllProducts([FromQuery]QueryParameters queryParameters)
         {
-            var products = await _context.Products.ToArrayAsync();
-            return Ok(products);
+            IQueryable<Product> products = _context.Products;
+
+            products = products
+                .Skip(queryParameters.Size * (queryParameters.Page - 1))
+                .Take(queryParameters.Size);
+
+            return Ok(await products.ToArrayAsync());
         }
 
         [HttpGet("{id}")]
---
+++ HPlusSport.API/Models/QueryParameters.cs
@@ -0,0 +1,17 @@
+﻿namespace HPlusSport.API.Models
+{
+    public class QueryParameters
+    {
+        private int _maxSize = 100;
+        private int _size = 50;
+        public int Page { get; set; } = 1;
+        public int Size
+        {
+            get { return _size; }
+            set
+            {
+                _size = Math.Min(_maxSize, value);
+            }
+        }
+    }
+}
